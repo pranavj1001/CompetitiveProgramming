31d6511a9e8c41f6341ca8b1f7252969d48aecbf
pranavj1001 <pranavj1001@gmail.com>
2016-10-04 23:37:28 +0530

    added JavaSingleton.java

---
+++ JavaSingleton.java
@@ -0,0 +1,18 @@
+//The Question for this solution can be found at https://www.hackerrank.com/challenges/java-singleton?h_r=next-challenge&h_v=zen
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.lang.reflect.*;
+
+
+class Singleton{
+    private Singleton() {}
+    public String str;
+    private static Singleton instance = new Singleton();
+    public static Singleton getSingleInstance() { 
+        return instance; 
+    }
+}
+
