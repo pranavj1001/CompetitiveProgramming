28c5b671d1b94724fe6dc1bc911b5eb571e84cfa
pranavj1001 <pranavj1001@gmail.com>
2016-12-07 19:34:01 +0530

    added StrangeGrid.java

---
+++ StrangeGrid.java
@@ -0,0 +1,24 @@
+//The Question for this solution can be found at https://www.hackerrank.com/challenges/strange-grid
+
+import java.util.Scanner;
+
+public class StrangeGrid {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        long row, column, x;
+        row = sc.nextInt();
+        column = sc.nextInt();
+        if(row % 2 == 0){
+            //number is odd
+            x = (5*row) - 9;
+        }else{
+            //number is even
+            x = (5*row) - 5;
+        }
+        x += (2 * column) - 2;
+        System.out.println(x);
+        sc.close();
+    }
+}
