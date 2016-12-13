627aae38b6ca7e848a5378fef40b0e7e0658b28a
pranavj1001 <pranavj1001@gmail.com>
2016-12-13 21:17:35 +0530

    updated IsFibo.java

--- IsFibo.java
+++ IsFibo.java
@@ -1,34 +1,32 @@
 //The Question for this solution can be found at https://www.hackerrank.com/challenges/is-fibo
 
+
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class IsFibo {
     
+    static HashSet<Long> fibs = new HashSet<Long>();
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    	/* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        long max = 10000000000l;
+        long a = 1, b = 1, c;
+        fibs.add(0l);
+        fibs.add(1l);
+        do{
+            c = a+b;
+            fibs.add(c);
+            a = b;
+            b = c;
+        }while(c <= max);
         Scanner sc = new Scanner(System.in);
         int testCases = sc.nextInt();
-        while(testCases-->0){
-            long n = sc.nextInt();
-            System.out.println(Fibonacci(n));
-        }
+        while(testCases-->0) 
+            System.out.println(isFib(sc.nextLong()));
         sc.close();
     }
     
-    public static String Fibonacci(long n) {
-        long fib1 = 0;
-        long fib2 = 1;
-        do {
-            long temp = fib1;
-            fib1 = fib2;
-            fib2 = temp + fib2;
-            }
-        while (fib2 < n);
-
-        if (fib2 == n)
-            return "IsFibo";
-        else
-            return "IsNotFibo";
-    }
-    
+    private static String isFib(long n){
+        return fibs.contains(n)? "IsFibo":"IsNotFibo";
+    }  
 }
