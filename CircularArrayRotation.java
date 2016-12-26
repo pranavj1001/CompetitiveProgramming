9e95fe2cf1f23e68b47f38b926bfa3afd0ab402e
pranavj1001 <pranavj1001@gmail.com>
2016-12-26 20:02:34 +0530

    update CircularArrayRotation.java
    improved the time efficiency significantly

--- CircularArrayRotation.java
+++ CircularArrayRotation.java
@@ -1,5 +1,6 @@
 //The Question for this solution can be found at https://www.hackerrank.com/challenges/circular-array-rotation
 
+
 import java.util.Scanner;
 
 public class CircularArrayRotation {
@@ -10,33 +11,19 @@ public class CircularArrayRotation {
         int k = in.nextInt();
         int q = in.nextInt();
         int[] array = new int[n];
-        int[] queries = new int[q];
+        int rotations = k%n;
         for(int a_i=0; a_i < n; a_i++){
             array[a_i] = in.nextInt();
         }
         for(int a0 = 0; a0 < q; a0++){
-            queries[a0] = in.nextInt();
+           int m = in.nextInt();
+           if(m - rotations >= 0)
+              System.out.println(array[m - rotations]);
+           else
+            System.out.println(array[m - rotations + array.length]);
         }
-        
-        for(int i = 0; i < k; i++)
-            performCircularRotation(array, n);
-        
-        for(int i = 0; i < q; i++)
-            System.out.println(array[queries[i]]);
-        
-        in.close();
-        
+        in.close();                 
     }
-    
-    private static void performCircularRotation(int[] array, int n){
-        int[] newArray = new int[n];
-        for(int i = 0; i < n-1; i++){
-            newArray[i+1] = array[i];
-        }
-        newArray[0] = array[n-1];
-        for(int i = 0; i < n; i++){
-            array[i] = newArray[i];
-        }
-    }
+        
     
 }
