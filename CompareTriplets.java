71b3e78eae5ce87ff7435ff31777ace22b596eb7
Pranav Jain <pranavj1001@gmail.com>
2018-05-18 23:46:29 +0530

    add CompareTriplets.java

---
+++ CompareTriplets.java
@@ -0,0 +1,69 @@
+// Question: https://www.hackerrank.com/challenges/compare-the-triplets/submissions
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the solve function below.
+    static int[] solve(int[] a, int[] b) {
+        int[] points = new int[2];
+        points[0] = 0;
+        points[1] = 0;
+        for(int i = 0; i < a.length; i++) {
+            if(a[i] > b[i]) {
+                points[0]++;
+            } else if(b[i] > a[i]) {
+                points[1]++;
+            }
+        }
+        return points;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int[] a = new int[3];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 3; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        int[] b = new int[3];
+
+        String[] bItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 3; i++) {
+            int bItem = Integer.parseInt(bItems[i]);
+            b[i] = bItem;
+        }
+
+        int[] result = solve(a, b);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
