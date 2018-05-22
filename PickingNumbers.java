c08c7aae36b9d8df343769b2848f786fb0075af7
Pranav Jain <pranavj1001@gmail.com>
2018-05-22 23:56:41 +0530

    add PickingNumbers.java

---
+++ PickingNumbers.java
@@ -0,0 +1,62 @@
+// Question: https://www.hackerrank.com/challenges/picking-numbers/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the pickingNumbers function below.
+    static int pickingNumbers(int[] a) {
+        int max = 0;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i : a) {
+            if(map.containsKey(i)) {
+                map.put(i, map.get(i) + 1);
+            } else {
+                map.put(i, 1);
+            }
+        }
+        for(int i : a) {
+            int current = map.get(i);
+            int previous = 0;
+            if(map.containsKey(i-1))
+                previous = map.get(i-1);
+            if((current + previous) > max)
+                max = current + previous;
+        }
+        return max;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        int result = pickingNumbers(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
