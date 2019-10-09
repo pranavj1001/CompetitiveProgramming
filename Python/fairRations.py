81504c2f2fa5fb9b5ba2015f415d40b8bd399c35
Pranav Jain <pranavj1001@gmail.com>
2019-10-09 23:14:09 +0530

    add fairRations.py

---
+++ Python/fairRations.py
@@ -0,0 +1,16 @@
+def fairRations(B):
+	
+	if sum(B) % 2 == 1:
+		return 'NO'
+	else:
+		count_1 = 0
+		for i in range(0, len(B) - 1):
+			if B[i] % 2 == 1:
+				count_1 += 2
+				B[i] += 1
+				B[i+1] += 1
+				
+		return count_1
+	
+B = list(map(int, input().rstrip().split()))
+print(fairRations(B))
